db5945cf8c07596c1c2da9aae448ddc5d1562433
Remi-Potvin <66944646+Remi-Potvin@users.noreply.github.com>
2020-10-14 15:16:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 const app = Vue.createApp({
   data() {
-    return {
-      counter: 0,
-      name: "",
-    };
+    return {};
   },
-  methods: {
-    add() {
-      this.counter++;
-    },
-    remove(num) {
-      this.counter -= num;
-    },
-    // remove() {
-    //   this.counter--;
-    // },
-    setName(event, lastName) {
-      this.name = `${event.target.value} ${lastName}`;
-    },
-    // Si on n'envois pas de valeur dans le setName, rien n'empèche de récupérer cette valeur pour la renvoyer de manière dynamique avec le target.value  target.value = ce qu'on tape // this.name = name dans data() ==> ce qu'on tape en temps réel est renvoyé dans name
-    submitForm(e) {
-      e.preventDefault();
-      alert(`Hey submitted ! Thx ${this.name}`);
-    },
-    // Pour le rprevent defaut ou on peut soit le mettre dans la fonction soit l'écrire directement dans l'HTML => v-on:submit.prevent=
-  },
+  methods: {},
 });
 
 app.mount("#root");
